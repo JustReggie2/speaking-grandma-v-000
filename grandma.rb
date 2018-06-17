3a693ed8cfa05d8f9139db498df17ce983473e95
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-06-17 17:54:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "#{phrase}".downcase
-  "HUH?! SPEAK UP, SONNY!"
-  else phrase == "#{phrase}".upcase
+  if phrase.upcase
     "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
 
